62fb274f1bf9837afb3d7d4e60ae673c81a74a8d
nks738 <nathankshin@gmail.com>
2020-11-23 18:47:59 -0800

    pushed correct version

--- main.cpp
+++ main.cpp
@@ -18,19 +18,41 @@ void menu() {
     int choice;
     cout << "Please enter your choice: " << endl << endl;
 
-    cout << "Print top 10 by rating - 1" << endl;
+    cout << "Print by Score - 1" << endl;
     cout << "Print top 10 by actor/actress - 2" << endl;
     cout << "Print top 10 by genre - 3" << endl;
+    cout << "Print by genre - 4" << endl;
+
     cin >> choice;
 
-    switch (choice){
-        case 1: printMoviesRating();
-                break;
+    if(choice == 1) {
+        cout << "Print top 10 movies - 1" << endl;
+        cout << "Print top 10 most recent - 2" << endl;
+        cin >> choice;
+
+        switch(choice) {
+            case 1: printTop10(); break;
+            case 2: printTop10Recent(); break;
+        }
+    }
+
+    if (choice == 2) {
+        string actor;
+        cout << "Enter actor/actress name" << endl;
+        cin >> actor;
+        printMoviesByActor(actor);
+    }
+    
+    if(choice == 3) {
         
-        case 2: printMoviesByActor(); break;
-        case 3: printMoviesByGenre(); break;
-
-        default: break;
-
-    } 
+    }
+    
+    if(choice == 4){ 
+       
+        cout << "Print top 10 movies - 1" << endl; 
+        cout << "Print top 10 most recent - 2" << endl;
+        cout << "Print top ";
+        cin >> choice; 
+        
+    }
 }
