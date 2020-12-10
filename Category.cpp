ac0a138ab79b071923d30de84be55eba1948a369
HenryKZheng <57116759+HenryKZheng@users.noreply.github.com>
2020-12-10 07:21:01 -0800

    Update Category.cpp

--- Category.cpp
+++ Category.cpp
@@ -25,7 +25,6 @@ void Category::addMovie(Movie* movie_to_add){
 void Category::addCategory(Category* category_to_add){
   this->category_List.push_back(category_to_add);
 }
-
 void Category::setTitle(string title_to_add){
   this->title =  title_to_add; 
 }
