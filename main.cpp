b161754f3717cbfb80e6faafdb5de3e5308cc605
Nathanael Shin <59461068+nks738@users.noreply.github.com>
2020-12-10 06:27:44 -0800

    Added main.cpp

--- main.cpp
+++ main.cpp
@@ -1,140 +1,203 @@
 #include <iostream>
 
-using namespace std;
+#include "Movie.hpp"
+#include "Media.hpp"
+#include "Category.hpp"
+#include "Factory.hpp"
+#include "DisplayStrategy.hpp"
+#include <vector>
+#include <map>
+#include <string>
+using namespace std; 
+class Movie; 
+/*function signatures*/
 
-void menu();
+void menu(Factory*);
+void sortMovies(vector<Movie*> &vec);
+int partition(vector<Movie*> &values, int left, int right);
+void quicksort(vector<Movie*> &values, int left, int right);
+void recMovie(vector<Movie*> values, string movie_name, map<string, Movie*> movMap);
+
 
 int main() {
-    menu();
-    return 0;
+  
+  /*Create factory object and parse data*/  
+  Factory* movieParse = new Factory();
+  movieParse->parseMovieData();
+
+  /*sorting movies by score*/
+  vector<Movie*> temp = movieParse->getMovieVec();
+  sortMovies(temp);
+  movieParse->setMovieVec(temp);
+  
+  menu(movieParse);
+  
+  
+  return 0; 
+  
 }
 
-void menu() {
+void menu(Factory* movie) {
     int choice;
-    cout << "Please enter your choice: " << endl << endl;
+     
+  do{
+      cout << "Please enter your choice: " << endl << endl;
+      cout << "Print by Score - 1" << endl;
+      cout << "Print by Actor - 2" << endl;
+      cout << "Print by Genre - 3" << endl;
+      cout << "Recommend Movie - 4" << endl;
+      cout << "Print Most Recent - 5" << endl;
+      cout << "Print by Director - 6" << endl; 
+      cout << "Quit - 9" << endl;
+      
+      cin >> choice;
 
-    cout << "Print by Score - 1" << endl;
-    cout << "Print top 10 by actor/actress - 2" << endl;
-    cout << "Print top 10 by genre - 3" << endl;
-    cout << "Print by genre - 4" << endl;
-    cout << "Recommend Movie - 5" << endl;
+      if(choice == 1) {
+          movie->setFilterFunction(new FilterTopTen());
+          movie->filter();
+      }
 
-    cin >> choice;
+      else if (choice == 2) {
+          string actor;
+          cout << "Enter actor/actress name." << endl;
+          cin.ignore();
+          getline(cin, actor);
+          movie->setFilterFunction(new FilterActor(actor));
+          movie->filter();
+      }
+      else if (choice == 5){
+        int year;
+        cout << "Enter year of production." << endl;
+        cin >> year;
+        movie->setFilterFunction(new FilterTopTenRecent(year));
+        movie->filter();
+      }
+      
+      else if(choice == 3) {
+          string genre;
+          cout << "Please enter the genre." << endl;
+          cin >> genre;
+          movie->setFilterFunction(new FilterGenre(genre));
+          movie->filter();
+      }
+      else if(choice == 6) {
+          string director;
+          cout << "Please enter the director." << endl;
+          cin.ignore();
+          getline(cin, director);
+          movie->setFilterFunction(new FilterDirector(director));
+          movie->filter();
+      }
 
-    if(choice == 1) {
-        cout << "Print top 10 movies - 1" << endl;
-        cout << "Print top 10 most recent - 2" << endl;
-        cin >> choice;
+      else if(choice ==4){
+        string movName;
+        cout << "What movies do you like?\nWe will find some others you might like!" << endl;
+        cin.ignore();
+        getline(cin, movName);
 
-        switch(choice) {
-            case 1: printTop10(); break;
-            case 2: printTop10Recent(); break;
-        }
-    }
-
-    if (choice == 2) {
-        string actor;
-        cout << "Enter actor/actress name" << endl;
-        cin >> actor;
-        printMoviesByActor(actor);
-    }
-    
-    if(choice == 3) {
-        string genre;
-        string answer;
-        bool flag = true;
-        cout << "Please enter the genre" << endl;
-        cin >> genre;
-        cout << "Do you have a sub-genre? (y/n)" << endl;
-        cin >> answer;
-        while(flag) {
-            if(answer == "y") {
-                string subgenre;
-                cout << "Please enter sub-genre" << endl;
-                cin >> subgenre;
-                printMoviesGenreWithSub(genre, subgenre);
-                flag = false;
-            }
-            else if(answer == "n") {
-                printMoviesByGenre(genre);
-                flag = false;
-            }
-            else {
-                cout << "Please enter a valid response, (y/n)" << endl;
-            }
-        }
-    }
-    
-    if(choice == 4){ 
-        cout << "Print top 10 movies - 1" << endl; 
-        cout << "Print top 10 most recent - 2" << endl;
-        cout << "Print top ";
-        cin >> choice; 
+        /*check if movie is in dataset*/
+       if(!(movie->movieExists(movName))){
+          cout << "Sorry, we do not have any records of this movie." << endl << endl;
+       }
+       else{
+          
+          recMovie(movie->getMovieVec(), movName, movie->getMovieMap());
+          
+       }
         
-    }
+      }
+      else{
+        cout << "Invalid Input: Choose 1-5" << endl << endl;
+      }
+  }while(choice != 9);
+}
 
-    if(choice == 5) {
-        string recType;
-        bool flag1 = true;
-        cout << "Would you like to choose a specific way to get a recommendation? (y/n)" << endl;
-        cin >> recType;
-        while(flag1) {
-            if(recType == "n") {
-                string movie;
-                cout << "Please enter your movie" << endl;
-                cin >> movie;
-                while(!checkMovieInList(movie)) {
-                    cout << "Movie not found. Please try again. " << endl;
-                    cin >> movie;
-                }
-                recommendMove(movie);
+       
+void sortMovies(vector<Movie*> &vec){
+  quicksort(vec, 0, vec.size()-1);
+}
 
-            }
-            else if(recType == "y") {
-                string movieRecChoice;
-                int recChoice;
-                bool choiceFlag = true;
-                cout << "Please enter your movie" << endl;
-                cin >> movieRecChoice;
-
-                while(!checkMovieInList(movieRecChoice)) {
-                    cout << "Movie not found. Please try again." << endl;
-                    cin >> movieRecChoice;
-                }
-
-                while(choiceFlag) {
-                    cout << "How would you like to get a recommendation?" << endl << endl;
-                    cout << "Genre - 1" << endl;
-                    cout << "Actor/actress - 2" << endl;
-                    cout << "Rating - 3" << endl;
-                    cin >> recChoice;
-
-                    if(recChoice == 1) {
-                        string recGenre;
-                        cout << "Enter genre" << endl;
-                        cin >> recGenre;
-                        movieRecGenre(movieRecChoice, recGenre);
-                        choiceFlag = false;
-                    }
-                    else if(recChoice == 2) {
-                        string recActor;
-                        cout << "Enter actor/actress name" << endl;
-                        cin >> recActor;
-                        movieRecActor(movieRecChoice, recActor);
-                        choiceFlag = false;
-                    }
-                    else if(recChoice == 3) {
-                        double rating;
-                        cout << "Please enter the rating of your movie" <<endl;
-                        cin >> rating;
-                        movieRecRating(movieRecChoice, rating);
-                        choiceFlag = false;
-                    }
-                    else {
-                        cout << "Invalid response please try again" << endl;
-                    }
-                }
-            }
+int partition(vector<Movie*> &values, int left, int right) {
+    double pivotIndex = left + (right - left) / 2;
+    double pivotValue = values.at(pivotIndex)->getRating();
+    double i = left, j = right;
+    Movie* temp;
+    while(i <= j) {
+        while(values.at(i)->getRating() > pivotValue) {
+            i++;
+        }
+        while(values.at(j)->getRating() < pivotValue) {
+            j--;
+        }
+        if(i <= j) {
+            temp = values[i];
+            values[i] = values[j];
+            values[j] = temp;
+            i++;
+            j--;
         }
     }
+    return i;
+}
+
+void quicksort(vector<Movie*> &values, int left, int right) {
+    if(left < right) {
+        int pivotIndex = partition(values, left, right);
+        quicksort(values, left, pivotIndex - 1);
+        quicksort(values, pivotIndex, right);
+    }
+}
+
+
+void recMovie(vector<Movie*> values, string movie_name, map<string, Movie*> movMap){
+ 
+  Movie* origMovie =  movMap.find(movie_name)->second;
+
+  /*-------------------------------*/
+  /*Start checking for similarities*/
+  /*-------------------------------*/
+  
+  
+  int movieCounter = 0;
+  int whileCounter = 0;
+  vector<string> origMovieActors = origMovie->getActors();
+  vector<string> origMovieGenres = origMovie->getGenres();
+  cout << endl<< "We think you would enjoy:" << endl << endl;
+  
+  for(unsigned int i = 0; i < values.size() && movieCounter < 3; ++i){
+      int similarity_count = 0; 
+    if(movie_name == values.at(i)->getTitle()){
+      i++;
+    }
+    vector<string> temp = values.at(i)->getActors();
+    for(int j = 0; j < origMovieActors.size(); j++){
+      for(int k = 0; k < temp.size(); k++ ) {
+          if(origMovieActors.at(j) == temp.at(k)){
+                similarity_count++; 
+          }
+      }
+    }
+
+    if(values.at(i)->getDirector() == origMovie->getDirector()){
+      similarity_count++;
+    }
+
+     vector<string> temp2 = values.at(i)->getGenres();
+    for(int j = 0; j < origMovieGenres.size(); j++){
+      for(int k = 0; k < temp2.size(); k++ ) {
+          if(origMovieGenres.at(j) == temp2.at(k)){
+                similarity_count++; 
+          }
+      }
+    }
+
+    if(similarity_count >= 3) {
+      values.at(i)->print();
+      cout << endl;
+      movieCounter++;
+    }
+  
+  }
+  
+  
 }
